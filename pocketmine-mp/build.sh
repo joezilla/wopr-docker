9d0a8209d9fd7217abd09e40b988773ea90c3715
jochen <jochen@Megalon.jtoee.local>
2021-01-10 23:50:54 -0500

    updates

--- pocketmine-mp/build.sh
+++ pocketmine-mp/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --pull -t joezilla/pocketmine:latest -f 3.14/Dockerfile . && \
+docker build --pull -t joezilla/pocketmine:latest -f 3.15/Dockerfile . && \
 docker push joezilla/pocketmine:latest
